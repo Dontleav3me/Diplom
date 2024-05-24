caac8c8e30997a1d6386839e720420a0c125d50f
F1ower <Daniil.shirkin005@gmail.com>
2024-05-24 10:02:28 +0300

    123

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'mainapp',
+    'mainapp', # Добавление приложение в настройки проекта
 ]
 
 MIDDLEWARE = [
