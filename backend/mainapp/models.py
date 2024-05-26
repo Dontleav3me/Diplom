e7139cbeffe4a645d43a56b3ddd487b8541a6cd3
F1ower <Daniil.shirkin005@gmail.com>
2024-05-27 00:02:28 +0300

    123

--- backend/mainapp/models.py
+++ backend/mainapp/models.py
@@ -3,3 +3,10 @@ from django.db import models
 class project(models.Model):
     name = models.CharField(max_length=256)
     description = models.CharField(max_length=1024)
+
+class Directions(models.Model):
+    name = models.CharField(max_length=256)
+    image = models.ImageField(upload_to='images/')
+
+class last_news(models.Model):
+    
